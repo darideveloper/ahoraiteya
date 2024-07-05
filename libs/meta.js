d4172f11d72574db6d98dda0d4ccdb73305b5ea8
daridev <darideveloper@gmail.com>
2024-07-05 09:43:16 -0600

    feat (index): add meta tags

---
+++ libs/meta.js
@@ -0,0 +1,5 @@
+export const metaData = {
+  title: "AhoraiteYA!",
+  description: " Servicios de logística a un click de distancia.",
+  keywords: "delivery, empresas, transporte, compras, comerciosm",
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,11 +6,20 @@ import WhyChooseUs from "@/sections/why-choose-us"
 import DownloadApp from "@/sections/download-app"
 import JoinUs from "@/sections/join-us"
 import Contact from "@/sections/contact"
+import Head from 'next/head'
+import { metaData } from "@/libs/meta"
 
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>{metaData.title}</title>
+        <meta name="description" content={metaData.description} />
+        <meta name="keywords" content={metaData.keywords} />
+        <meta name="author" content={metaData.author} />
+        <link rel="icon" type="image/x-icon" href="/web/favicon.ico"></link>
+      </Head>
       <Hero />
       <Benefits />
       <Experience />
